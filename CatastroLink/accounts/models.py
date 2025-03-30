b0985d65b65f74ad6fed17afd9910fbf95c4379a
Kenneth Mejia <kmejia@students.cs.ubc.ca>
2025-03-29 18:35:28 -0700

    add custom User model

--- CatastroLink/accounts/models.py
+++ CatastroLink/accounts/models.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class AppUser(AbstractUser):
+    latitude = models.DecimalField(
+        max_digits=9, decimal_places=6, null=True, blank=True
+    )
+    longitude = models.DecimalField(
+        max_digits=9, decimal_places=6, null=True, blank=True
+    )
