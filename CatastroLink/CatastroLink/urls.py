542faa8dc7d988a5921e809bf381d3f2daa426e8
Kenneth Mejia <kmejia@students.cs.ubc.ca>
2025-03-30 09:50:15 -0700

    remove unused url

--- CatastroLink/CatastroLink/urls.py
+++ CatastroLink/CatastroLink/urls.py
@@ -1,15 +1,14 @@
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import path, include
-from .views import home, disaster_alert_view
+from .views import home
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("accounts.urls")),
     path("", home, name="home"),
-    path("disaster_alert/", disaster_alert_view, name="disaster_alert"),
     path("reports/", include("reports.urls")),
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
     path("preparation/", include("preparation.urls")),
-    path("disaster_mode/", include("disaster_mode.urls"))
+    path("disaster_mode/", include("disaster_mode.urls")),
 ]
