b01fdff97282d299625dbcd42eea11bff547a6ed
Kenneth Mejia <kmejia@students.cs.ubc.ca>
2025-03-30 09:21:48 -0700

    fix ui merge

--- CatastroLink/CatastroLink/urls.py
+++ CatastroLink/CatastroLink/urls.py
@@ -1,13 +1,12 @@
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import path, include
-from .views import home, disaster_alert_view
+from .views import home
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("accounts.urls")),
     path("", home, name="home"),
-    path("disaster_alert/", disaster_alert_view, name="disaster_alert"),
     path("reports/", include("reports.urls")),
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
     path("preparation/", include("preparation.urls")),
